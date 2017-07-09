678f962549045f7755b871529dd28abc7a02f7ec
Matthew Tadd <matt.tadd@gmail.com>
2017-07-09 15:09:36 -0400

    bootstrapped

---
+++ _assets/js/main.js
@@ -0,0 +1,13 @@
+//=require bootstrap
+
+$( document ).ready( function() {
+    $('ul.navbar-nav li.dropdown').hover(
+        function() {
+            $(this).find('.dropdown-menu')
+                .stop(true, true).delay(100).fadeIn(300);
+        },
+        function() {
+            $(this).find('.dropdown-menu')
+                .stop(true, true).delay(100).fadeOut(300);
+    });
+});
